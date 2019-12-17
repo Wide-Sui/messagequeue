00a390b66a5d19869c1314129ef1d83779efe0fa
suiweikuan <swksjlx@foxmail.com>
2019-12-17 15:46:40 +0800

    添加一点备注

--- src/main/java/io/openmessaging/DefaultQueueStoreImpl.java
+++ src/main/java/io/openmessaging/DefaultQueueStoreImpl.java
@@ -26,10 +26,11 @@ public class DefaultQueueStoreImpl extends QueueStore {
     public Map<String, List<Block>> blockMap = new ConcurrentHashMap<>();
 
     public Map<String, DataCache> cacheMap = new ConcurrentHashMap<>();
-    //父目录路径
-    public static final String DIRPATH = "/home/maskwang/Myfolder/message/data/";
 
-    Lock lock = new ReentrantLock();
+    //父目录路径
+    public static final String DIRPATH = "./data/";
+
+    Lock lock = new ReentrantLock();//可重入锁
 
     //预先创建根目录
     static {
@@ -40,8 +41,11 @@ public class DefaultQueueStoreImpl extends QueueStore {
     }
 
     public void put(String queueName, byte[] message) {
+        //获得在哪个文件中
         int hash = hashFile(queueName);
+        //为每个文件新建一个地址，一共16个文件
         String path = DIRPATH + hash + ".txt";
+        //上锁
         lock.lock();
         //创建文件
         File file = new File(path);
@@ -52,19 +56,24 @@ public class DefaultQueueStoreImpl extends QueueStore {
                 e.printStackTrace();
             }
         }
-
+        
+        //如果该队列没有在blockMap中就将其
         if (!blockMap.containsKey(queueName)) {
             List<Block> list = new ArrayList();
             blockMap.put(queueName, list);
         }
 
+        //如果该队列没在缓冲map中
         if (!cacheMap.containsKey(queueName)) {
             DataCache dataCache = new DataCache();
             cacheMap.put(queueName, dataCache);
         }
 
+        //获取该队列的缓存文件
         DataCache dataCache = cacheMap.get(queueName);
+        //当缓存的数量达到10的时候
         if (dataCache.count == 10) {
+            //将数据写入
             FileChannel fileChannel = null;
             // long fileLength = 0;
             try {
@@ -76,7 +85,10 @@ public class DefaultQueueStoreImpl extends QueueStore {
 
             long blockPosition;
             try {
+                //FileChannel实例的size()方法将返回该实例所关联文件的大小，为什么要调用两次getLeastBlockPosition函数
+                //这里可能是作者测试没有通过，可能是因为缓存的大小超过了1024
                 blockPosition = getLeastBlockPosition(getLeastBlockPosition(fileChannel.size()));
+                //position()方法获取FileChannel的当前位置
                 Block block = new Block(blockPosition, dataCache.dataBuffer.position());
                 block.size = 10;
                 blockMap.get(queueName).add(block);
@@ -158,6 +170,7 @@ public class DefaultQueueStoreImpl extends QueueStore {
     }
 
 
+    //offset和num的意义是什么？
     public Collection<byte[]> get(String queueName, long offset, long num) {
 
         //队列不存在
@@ -166,24 +179,27 @@ public class DefaultQueueStoreImpl extends QueueStore {
         }
         //消息集合
         List<byte[]> msgs = new ArrayList();
+        //blocks是所有的的block的列表
         List<Block> blocks = blockMap.get(queueName);
 
+        //找到存放改队列数据的文件
         int hash = hashFile(queueName);
         String path = DIRPATH + hash + ".txt";
         FileChannel fileChannel = null;
+        //用于寻找block的位置
         int size = blocks.get(0).size;
         int eleNum = 0;
         //记录了目标block所在的下标
         int blockNum = 0;
         lock.lock();
         try {
+            //新建读取的filechannel，block的size一般都是10
             fileChannel = new RandomAccessFile(new File(path), "rw").getChannel();
             for (int i = 1; i < blocks.size() && size < offset; i++, blockNum++) {
                 size += blocks.get(i).size;
             }
-
+            //向后退回一个就是要找的block
             size = size - blocks.get(blockNum).size;
-
 
             for (int i = blockNum; i < blocks.size(); i++) {
                 //size+=blocks.get(i).size;
@@ -231,12 +247,14 @@ public class DefaultQueueStoreImpl extends QueueStore {
 
 
     //根据队列的名字hash到对应的文件中,共16个文件
+    //个人觉得这里应该是只取最后8位，共256个文件吧？？？
+    //应该是return queueName.hashCode() & 0x0f;
     int hashFile(String queueName) {
         return queueName.hashCode() & 0xff;
         //return 0;
     }
 
-    //block的大小为2048
+    //block的大小为1024，根据当前文件已经存在的写的位置,找到下一个比该位置大的,且是1024的倍数
     public long getLeastBlockPosition(long length) {
         if (length == 0) {
             return 0;
